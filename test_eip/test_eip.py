0c9c3bfb9a414f22863909cc1cdac242f4f9269f
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-05-13 14:41:44 +0200

    Add EIP test

---
+++ test_eip/test_eip.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from scapy.layers.inet6 import IPv6, IPv6ExtHdrHopByHop
+from scapy.layers.eip import EIP, EIPShortIdentifier, EIPIEUnknown
+from scapy.layers.l2 import Ether
+from scapy.utils import wrpcap
+
+
+if __name__ == '__main__':
+    #pkt = Ether(src='90:e2:ba:84:d5:ec', dst='90:e2:ba:84:d7:78') / IPv6(src='faaa::1', dst='faaa::2') / IPv6ExtHdrHopByHop(options=[EIP()])
+    pkt = Ether(src='90:e2:ba:84:d5:ec', dst='90:e2:ba:84:d7:78') / IPv6(src='faaa::1', dst='faaa::2') / IPv6ExtHdrHopByHop(options=[EIP(ielems=[EIPShortIdentifier(), EIPShortIdentifier()])])
+    wrpcap('eip.pcap', pkt, append=False)
