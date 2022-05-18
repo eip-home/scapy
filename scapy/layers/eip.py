d63e66ed5aafe6db1e7ef6aca34dc61a6a17b9da
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-05-18 13:38:57 +0200

    Add support for Long Identifier information element
    Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

--- scapy/layers/eip.py
+++ scapy/layers/eip.py
@@ -3,7 +3,7 @@
 from scapy.compat import orb
 from scapy.error import Scapy_Exception
 from scapy.packet import Packet
-from scapy.fields import BitFieldLenField, ByteEnumField, FieldLenField, NBytesField, \
+from scapy.fields import BitFieldLenField, ByteEnumField, ConditionalField, FieldLenField, NBytesField, \
     ShortField, StrLenField, BitField, PacketListField, \
     ShortEnumField, ByteField, IntField, XNBytesField, XStrLenField
 from scapy.layers.inet6 import _hbhopts, _hbhoptcls, _OptionsField, _OTypeField
@@ -25,13 +25,28 @@ class MCDStackInvalidLengthField(Scapy_Exception):
     pass
 
 
+class LongIdentifierInvalidLengthField(Scapy_Exception):
+    """
+    basic frame structure not standard conform
+    (missing TLV, invalid order or multiplicity)
+    """
+    pass
+
+
 _eipiels_base = {
     0x01: "Short Identifier"
 }
 
 _eipiels_ext = {
     0x0001: "HMAC",
-    0x0002: "CPT"
+    0x0002: "CPT",
+    0x0003: "Long Identifier"
+}
+
+idtypes = {
+    0: "Default",
+    1: "Sequence Number only",
+    2: "Sequence Number and Generic Long Identifier"
 }
 
 # class HMACField(StrLenField):
@@ -72,6 +87,43 @@ class EIPShortIdentifier(Packet):
 
     def extract_padding(self, p):
         return b"", p
+    
+    
+class EIPLongIdentifier(Packet):
+
+    name = "EIP Long Identifier"
+    fields_desc = [
+        BitField("code", 2, 2),
+        BitFieldLenField("len", None, 6, length_of="id", adjust=lambda _,x: int((x+4)/4)),
+        ShortEnumField("type", 0x0003, _eipiels_ext),
+        ByteEnumField("idtype", 0, idtypes),
+        ConditionalField(IntField("seqnum", 0), lambda pkt:pkt.idtype in [1, 2]),
+        ConditionalField(StrLenField("id", "", length_from=lambda pkt: 0 if pkt.len is None else (pkt.len * 4)-4), lambda pkt:pkt.idtype in [0, 2])
+    ]
+
+    def extract_padding(self, p):
+        return b"", p
+
+    def _check(self):
+        """
+        run layer specific checks
+        """
+        
+        if self.id is not None:
+            id_len = len(self.id)
+            if id_len % 4 != 0:
+                raise LongIdentifierInvalidLengthField(
+                    'long identifier must be in multiples of 4 octets - '
+                    'got long identifier of size {}'.format(id_len))
+
+
+    def post_dissect(self, s):
+        self._check()
+        return super().post_dissect(s)
+
+    def do_build(self):
+        self._check()
+        return super().do_build()
     
     
 class EIPCPT(Packet):
@@ -281,7 +333,8 @@ _eipiels_cls = {
 
 _eipiels_ext_cls = {
     0x0001: EIPHmac,
-    0x0002: EIPCPT
+    0x0002: EIPCPT,
+    0x0003: EIPLongIdentifier,
 }
 
 
--- test_eip/test_eip.py
+++ test_eip/test_eip.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 
 from scapy.layers.inet6 import IPv6, IPv6ExtHdrHopByHop
-from scapy.layers.eip import EIP, EIPCPT, EIPHmac, EIPShortIdentifier
+from scapy.layers.eip import EIP, EIPCPT, EIPHmac, EIPShortIdentifier, EIPLongIdentifier
 from scapy.layers.l2 import Ether
 from scapy.utils import wrpcap
+
+
+def test_eip_long_identifier():
+    pkt = Ether(src='90:e2:ba:84:d5:ec', dst='90:e2:ba:84:d7:78') / IPv6(src='faaa::1', dst='faaa::2') / \
+                IPv6ExtHdrHopByHop(options=[EIP(ielems=[EIPLongIdentifier(idtype=2, seqnum=2**32-1, id=b"\x00\x01\x02\x03\x04\x05\x06\07")])])
+    wrpcap('eip_long_id.pcap', pkt, append=False)
+
 
 
 if __name__ == '__main__':
@@ -12,7 +19,8 @@ if __name__ == '__main__':
     #pkt = Ether(src='90:e2:ba:84:d5:ec', dst='90:e2:ba:84:d7:78') / IPv6(src='faaa::1', dst='faaa::2') / \
     #        IPv6ExtHdrHopByHop(options=[EIP(ielems=[EIPShortIdentifier(), EIPHmac()])])
     #        IPv6ExtHdrHopByHop(options=[EIP(ielems=[EIPShortIdentifier(), EIPShortIdentifier(), EIPShortIdentifier()])])
-    pkt = Ether(src='90:e2:ba:84:d5:ec', dst='90:e2:ba:84:d7:78') / IPv6(src='faaa::1', dst='faaa::2') / \
-            IPv6ExtHdrHopByHop(options=[EIP(ielems=[EIPHmac(hmac=b'\x00\x01\x02\x03\x04\x05\x06\x07')])])
+    #pkt = Ether(src='90:e2:ba:84:d5:ec', dst='90:e2:ba:84:d7:78') / IPv6(src='faaa::1', dst='faaa::2') / \
+    #        IPv6ExtHdrHopByHop(options=[EIP(ielems=[EIPHmac(hmac=b'\x00\x01\x02\x03\x04\x05\x06\x07')])])
     #        IPv6ExtHdrHopByHop(options=[EIP(ielems=[EIPCPT(mcdstack=b'\x00\x01\x02\x03\x04\x05\x06\x07')])])
-    wrpcap('eip_hmac.pcap', pkt, append=False)
+    #wrpcap('eip_hmac.pcap', pkt, append=False)
+    test_eip_long_identifier()
